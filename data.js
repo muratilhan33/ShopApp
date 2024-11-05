664fca6ebb719c7517b2ff37766e1fb61994738b
Murat İLHAN <muratilhan33@gmail.com>
2024-11-05 13:26:48 +0300

    data.json dosyası ile json server bağlantısı

---
+++ data.js
@@ -0,0 +1,21 @@
+module.exports = function () {
+    return {
+        products: [
+            { id: 1, name: "Samsung S5", price: 1000, imageUrl: '1.jpg', description: 'Kötü telefon' },
+            { id: 1, name: "Samsung S6", price: 1500, imageUrl: '2.jpg', description: 'İdare eder' },
+            { id: 1, name: "Samsung S7", price: 2000, imageUrl: '3.jpg', description: 'Fiyat performans' },
+            { id: 1, name: "Samsung S8", price: 2300, imageUrl: '4.jpg', description: 'Ortalama' },
+            { id: 1, name: "Samsung S9", price: 2700, imageUrl: '5.jpg', description: 'Üst segment' },
+            { id: 1, name: "Samsung S10", price: 3000, imageUrl: '6.jpg', description: 'Harika telefon' },
+            { id: 1, name: "Samsung S11", price: 3500, imageUrl: '7.jpg', description: 'Süper telefon' }
+        ],
+        categories: [
+            { id: 1, name: "Telefon" },
+            { id: 2, name: "Bilgisayar" },
+            { id: 3, name: "Elektronik" },
+        ],
+        orders: [
+
+        ]
+    }
+}
