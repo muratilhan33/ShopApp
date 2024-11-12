a4b697e91eac0d0a65b0fc87b1c726c101dd270f
Murat İLHAN <muratilhan33@gmail.com>
2024-11-12 21:51:46 +0300

    before storm

--- src/app/shop/shop.component.ts
+++ src/app/shop/shop.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { ProductRepository } from '../model/product.repository';
 import { CategoryRepository } from '../model/category.repository';
 import { Product } from '../model/product.model';
@@ -8,11 +8,12 @@ import { NgFor } from '@angular/common';
 import { PriceFormatPipe } from '../price-format.pipe';
 import { Cart } from '../model/cart.model';
 import { CartSummaryComponent } from "./cart-summary/cart-summary.component";
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'shop',
   standalone: true,
-  imports: [NavbarComponent, NgFor, PriceFormatPipe, CartSummaryComponent, CartSummaryComponent],
+  imports: [NavbarComponent, NgFor, PriceFormatPipe, CartSummaryComponent, CartSummaryComponent, FormsModule],
   templateUrl: './shop.component.html',
   styleUrl: './shop.component.css'
 })
@@ -21,17 +22,28 @@ export class ShopComponent {
   public selectedCategory: Category | null = null;
   public productsPerPage = 6;
   public selectedPage = 1;
+  public selectedProducts: Product[] = []; // Aktif sayfadaki ürünler
 
   constructor(
     private productRepository: ProductRepository,
     private categoryRepository: CategoryRepository,
     private cart: Cart
-  ) { }
+  ) {
+
+  }
+
+  ngOnInit(): void {
+    this.updateSelectedProducts();
+    this.changeCategory();
+  }
+
+  updateSelectedProducts(): void {
+    this.selectedProducts = this.productRepository.getProducts(this.selectedCategory);
+  }
 
   get products(): Product[] {
-    let index = (this.selectedPage - 1) * (this.productsPerPage);
-    return this.productRepository.getProducts(this.selectedCategory)
-      .slice(index, index + this.productsPerPage);
+    return this.selectedProducts
+      .slice(((this.selectedPage - 1) * (this.productsPerPage)), ((this.selectedPage - 1) * (this.productsPerPage)) + this.productsPerPage);
   }
 
   get pageNumbers(): number[] {
@@ -47,14 +59,26 @@ export class ShopComponent {
   }
 
   changeCategory(newCategory?: Category) {
+    this.updateSelectedProducts();
     this.selectedCategory = newCategory!;
+    this.changePage(1);
   }
 
   changePage(p: number) {
+    this.updateSelectedProducts();
     this.selectedPage = p;
+    console.log('page ' + p);
+    console.log('selected category ' + this.selectedCategory?.name);
+    console.log('prodcuts per page ' + this.productsPerPage);
   }
 
   addProductToCart(product: Product) {
     this.cart.addItem(product);
   }
+
+  changePageSize(size: number) {
+    this.productsPerPage = size;
+    this.changePage(1);
+  }
+
 }
