dfba6a959f92b82b7b50f2f92da81f5953c0b6b1
Murat İLHAN <muratilhan33@gmail.com>
2024-11-27 14:23:53 +0300

    Navbar media query

--- src/app/shop/navbar/navbar.component.ts
+++ src/app/shop/navbar/navbar.component.ts
@@ -1,18 +1,25 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { AuthService } from '../../model/auth.service';
-import { NgIf } from '@angular/common';
+import { NgClass, NgIf } from '@angular/common';
 @Component({
   selector: 'navbar',
   standalone: true,
-  imports: [RouterLink, NgIf],
+  imports: [RouterLink, NgIf, NgClass],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
   isLoggedIn: boolean;
+  screenWidth = window.innerWidth;
+
   constructor(private authService: AuthService) {
 
+
     this.isLoggedIn = this.authService.authenticated;
+
+    window.addEventListener('resize', () => {
+      this.screenWidth = window.innerWidth;
+    });
   }
 }
